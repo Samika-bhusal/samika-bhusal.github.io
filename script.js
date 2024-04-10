258b223c0cd6ef1ad5cd35b0eb6d6c7d6a6296a4
samikab <157431911+samikab@users.noreply.github.com>
2024-04-09 20:41:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,49 @@
-document.getElementById("contact").addEventListener("submit", function(event) {
-    event.preventDefault(); 
+document.addEventListener('DOMContentLoaded', function() {
+
+    // document.getElementById("contact").addEventListener("submit", function(event) {
+  //     event.preventDefault(); 
+    
+  //     var formData = new formData(this);
+    
+  //     localStorage.setItem("formData", JSON.stringify(formData));
+    
+  //     alert("Form submitted!");
+    
+  //   });
   
-    var formData = new formData(this);
   
-    localStorage.setItem("formData", JSON.stringify(formData));
+  const toggleSwitch = document.querySelector('#darkModeToggle');
+  const body = document.querySelector('body');
   
-    alert("Form submitted!");
+  //array of selectors for elements you want to toggle
+  const elementsToToggle = [
+    'my-resume h1',
+    'about-section h1',
+    '.contact-me label',
+    '.contact-me h1',
+    '.About'
+  ];
+  
+  toggleSwitch.addEventListener('change', switchTheme);
+  
+  function switchTheme() {
+    //line for debugging
+    console.log("Switching theme..."); 
+  
+      if (toggleSwitch.checked) {
+          //dark mode
+          body.classList.add('dark-mode');
+          elementsToToggle.forEach(selector => {
+              document.querySelector(selector).style.color = '#90b9ff';
+          });
+      } else {
+          //light mode
+          body.classList.remove('dark-mode');
+          elementsToToggle.forEach(selector => {
+              document.querySelector(selector).style.color = '#223952';
+          });
+      }
+  }
   
   });
+  
